adad7ec7158708bb4c9d0ac90feea2a2b17f8631
djkim81 <2pac81@sk.com>
2020-07-14 19:50:31 +0900

    first commit

---
+++ src/main/java/com/pilot/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.pilot.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static ApplicationContext applicationContext;
+	public static void main(String[] args) {
+		applicationContext = SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
